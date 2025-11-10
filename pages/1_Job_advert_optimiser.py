3e10827dc1839f84df0ba2c7f49c59eee8c300d5
Cab-mark <94869679+Cab-mark@users.noreply.github.com>
2025-11-10 12:48:45 +0000

    Fix typo in info message on Job Advert Optimiser page

--- pages/1_Job_advert_optimiser.py
+++ pages/1_Job_advert_optimiser.py
@@ -267,7 +267,7 @@ Text:
 # --------------------------
 
 st.title("Job advert optimiser (mock)")
-st.info('The hub could provide a front-end for JAO and could (if user rolepermits) enable publishing to the future Civil Service Jobs board.', icon="ℹ️")
+st.info('The hub could provide a front-end for JAO and could (if users role permits) enable publishing to the future Civil Service Jobs board.', icon="ℹ️")
 st.write(
     "Give me your job advert in **any format**. I’ll read it, work with you to improve it, and ask for missing information."
 )
